d5b539c6da8dc25a860fc39b118e31f3d0e03bf1
totenkopf1995 <uroxacin1@hotmail.com>
2024-04-14 02:09:42 -0500

    creacion

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use chrono::{Local, DateTime};
 use std::io::{self, Write};
+use std::env;
 
 fn main() {
 
@@ -13,4 +14,12 @@ fn main() {
 fn imprimir_fecha() {
     let ahora: DateTime<Local> = Local::now();
     println!("{}", ahora);
+}
+
+fn os() {
+    match env::consts::OS {
+        "android" => println!("Estás ejecutando Android!"),
+        "linux" => println!("Estás ejecutando Linux!"),
+        _ => println!("No estoy seguro de qué sistema operativo estás ejecutando."),
+    }
 }

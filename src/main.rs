fae5600a2446306af116a85c09c80977cbe3c5fb
totenkopf1995 <uroxacin1@hotmail.com>
2024-04-15 12:45:14 -0500

    creacion os

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use chrono::{Local, DateTime};
 use std::env;
 use std::io;
+use std::fs;
+use std::process::Command;
 
 fn main() {
 
@@ -21,6 +23,8 @@ fn main() {
     }
 
     println!("Respuesta válida: {}", input_char);
+    os();
+    imprimir_fecha();
 }
 
 fn imprimir_fecha() {
@@ -29,9 +33,27 @@ fn imprimir_fecha() {
 }
 
 fn os() {
-    match env::consts::OS {
-        "android" => println!("Estás ejecutando Android!"),
-        "linux" => println!("Estás ejecutando Linux!"),
+
+    let os = env::consts::OS;
+    let contents = fs::read_to_string("/etc/os-release")
+        .expect("Algo salió mal al leer el archivo");
+
+    match os {
+        "android" => {
+            println!("Estás ejecutando Android!");
+        },
+        "linux" => {
+            println!("Estás ejecutando Linux!");
+            if contents.contains("ubuntu") {
+                println!("Estás ejecutando Ubuntu!");
+            } else if contents.contains("debian") {
+                println!("Estás ejecutando Debian!");
+            } else if contents.contains("fedora") {
+                println!("Estás ejecutando Fedora!");
+            } else {
+                println!("No estoy seguro de qué distribución de Linux estás ejecutando.");
+            }
+        },
         _ => println!("No estoy seguro de qué sistema operativo estás ejecutando."),
     }
 }

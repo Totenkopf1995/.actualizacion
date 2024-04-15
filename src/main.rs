024b806dcdeb3369d580634e3dc173288b5c7116
totenkopf1995 <uroxacin1@hotmail.com>
2024-04-14 23:38:02 -0500

    modificacion while

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 use chrono::{Local, DateTime};
-use std::io::{self, Write};
 use std::env;
+use std::io;
 
 fn main() {
 
-    let mut input:char = ' ';
+    let mut input = String::new();
 
-    while input != 's' || input != 'n' {
+    println!("Desea Actualizar(s/n): ");
+    io::stdin().read_line(&mut input).expect("Error al leer la entrada");
 
+    // Convertir la entrada a un carácter único
+    let mut input_char = input.trim().chars().next().expect("No se ingresó ningún caracter");
+
+    // Comparar con caracteres individuales
+    while input_char != 's' && input_char != 'n' {
+        println!("Ingrese 's' para sí o 'n' para no:");
+        input.clear();
+        io::stdin().read_line(&mut input).expect("Error al leer la entrada");
+        input_char = input.trim().chars().next().expect("No se ingresó ningún caracter");
     }
+
+    println!("Respuesta válida: {}", input_char);
 }
 
 fn imprimir_fecha() {
